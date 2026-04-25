48c260d36c1379b0df68907d446ad60d56d1ebc8
holy21k <kidistteshome@72gmail.com>
2026-04-25 23:24:14 +0300

    feat(backend): register ClaimedIssue in django admin with filters and search

--- gitpulse-backend/issues/admin.py
+++ gitpulse-backend/issues/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import ClaimedIssue
 
-# Register your models here.
+
+@admin.register(ClaimedIssue)
+class ClaimedIssueAdmin(admin.ModelAdmin):
+    list_display    = ('title', 'repo_name', 'status', 'time_spent_secs', 'claimed_at')
+    list_filter     = ('status',)
+    search_fields   = ('title', 'repo_name', 'github_id')
+    ordering        = ('-claimed_at',)
+    readonly_fields = ('claimed_at',)
